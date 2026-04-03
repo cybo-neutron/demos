ea1331e93f4d72195d3b63324fc6e25177c70973
neer <neer@email.com>
2026-04-03 10:58:48 +0530

    test

--- video-tube/apps/api/src/app/routes/root.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-import movieRoute from "./movie.route";
-
-export default async function (fastify: FastifyInstance) {
-  fastify.register(movieRoute, {
-    prefix: "/v1/movie",
-  });
-}
--- video-tube/libs/database/src/repos/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./movie.repo.js";
-export * from "./genres.repo.js";
-export * from "./stackoverflow_tag.repo.js";
-export * from "./stackoverflow_question.repo.js";
-export * from "./stackoverflow_answer.repo.js";
-
